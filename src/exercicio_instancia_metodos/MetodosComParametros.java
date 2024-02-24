c329dd889f053efabb1bd3c98b48adef20f343b9
qaacademy <qaacademybr@gmail.com>
2024-02-24 12:19:09 -0300

    Exercicio metodos com parametro

--- src/exercicio_instancia_metodos/MetodosComParametros.java
+++ src/exercicio_instancia_metodos/MetodosComParametros.java
@@ -1,5 +1,12 @@
 package exercicio_instancia_metodos;
 
+//1- Criar uma classe;
+//2- Criar 5 metodos com retorno, recebendo parametros:
+//3- Criar 5 metodos sem retorno, recebendo parametros:
+// 4- Exibir o valor do(s) parametros enviados;
+//5- Fazer a chamada destes 10 métodos em um main.
+
+
 public class MetodosComParametros {
 
     public static void metodoUmSemRetorno(String nome){
