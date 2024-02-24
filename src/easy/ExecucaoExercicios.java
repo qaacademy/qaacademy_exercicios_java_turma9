6b4c3563d2e58ba4467709c035d7de8e8662c843
qaacademy <qaacademybr@gmail.com>
2024-02-24 15:19:15 -0300

    Exercicios Refatoração

---
+++ src/easy/ExecucaoExercicios.java
@@ -0,0 +1,8 @@
+package easy;
+
+public class ExecucaoExercicios {
+    public static void main(String[] args) {
+        Exercicio02_easy exercicio02Easy = new Exercicio02_easy();
+        System.out.println(exercicio02Easy.exibirPalavra("Estudar faz bem"));
+    }
+}
--- src/easy/Exercicio02_easy.java
+++ src/easy/Exercicio02_easy.java
@@ -4,10 +4,8 @@ import javax.swing.*;
 
 public class Exercicio02_easy {//Inicio Exercicio
 
-    public static void main(String[] args) {//Inicio main
-        String palavraDigitada;//Essa serve para armezar a palavra
-        palavraDigitada = JOptionPane.showInputDialog("Digite uma palavra");
-        System.out.println("Você digitou: " +palavraDigitada);
+    public String exibirPalavra(String palavraDigitada) {//Inicio main
+        return "Você digitou: " +palavraDigitada;
     }//Fim main
 
 }// Fim exercicio
