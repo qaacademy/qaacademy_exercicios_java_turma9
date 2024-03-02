0491e6a0096a5bb535a9c5234b1aec366853d69e
qaacademy <qaacademybr@gmail.com>
2024-03-02 10:07:19 -0300

    Refatoração ex5 easy

--- src/easy/ExecucaoExercicios.java
+++ src/easy/ExecucaoExercicios.java
@@ -13,8 +13,19 @@ public class ExecucaoExercicios {
 //        String texto = execicio03_ease.trocaValoresDigitados(primeiroValor, segundoValor);
 //        System.out.println(texto);
 
-        Exercicio04_easy exercicio04_easy = new Exercicio04_easy();
-        int numeroDigitado = Integer.parseInt(JOptionPane.showInputDialog("Digite um inteiro"));
-        System.out.println("O dobro do numero que você digitou é: "+ exercicio04_easy.calcularDobro(numeroDigitado)); // Exibe o valor da variavel com o um texto concatenado.
+//        Exercicio04_easy exercicio04_easy = new Exercicio04_easy();
+//        int numeroDigitado = Integer.parseInt(JOptionPane.showInputDialog("Digite um inteiro"));
+//        System.out.println("O dobro do numero que você digitou é: "+ exercicio04_easy.calcularDobro(numeroDigitado)); // Exibe o valor da variavel com o um texto concatenado.
+
+        int primeiroValor = Integer.parseInt(JOptionPane.showInputDialog("Digite o primeiro numero inteiro")); // Leitura do primeiro numero
+        int segundoValor = Integer.parseInt(JOptionPane.showInputDialog("Digite o segundo numero inteiro"));// Leitura do segundo numero
+        int terceiroValor = Integer.parseInt(JOptionPane.showInputDialog("Digite o terceiro numero inteiro"));// Leitura do terceiro numero
+
+        Exercicio05_easy exercicio05Easy = new Exercicio05_easy();
+
+        System.out.println("A soma dos numeros digitados é: " + exercicio05Easy.calcularSoma(primeiroValor, segundoValor, terceiroValor)); // Exibição soma
+        System.out.println("A subtração dos numeros digitados é: " + exercicio05Easy.calcularSubtracao(primeiroValor, segundoValor, terceiroValor)); // Exibição subtração
+        System.out.println("A multiplicação dos numeros digitados é:" + exercicio05Easy.calcularMultiplicacao(primeiroValor, segundoValor, terceiroValor));  // Exibição multiplicação
+        System.out.println("A média dos numeros digitados é:" + exercicio05Easy.calcularMedia(primeiroValor, segundoValor, terceiroValor));  // Exibição media
     }
 }
--- src/easy/Exercicio05_easy.java
+++ src/easy/Exercicio05_easy.java
@@ -4,23 +4,24 @@ import javax.swing.*;
 
 public class Exercicio05_easy {//Inicio
 
-    public static void main(String[] args) { // inicio main
-
-        int primeiroValor = Integer.parseInt(JOptionPane.showInputDialog("Digite o primeiro numero inteiro")); // Leitura do primeiro numero
-        int segundoValor = Integer.parseInt(JOptionPane.showInputDialog("Digite o segundo numero inteiro"));// Leitura do segundo numero
-        int terceiroValor = Integer.parseInt(JOptionPane.showInputDialog("Digite o terceiro numero inteiro"));// Leitura do terceiro numero
-
+    public Integer calcularSoma(int primeiroValor, int segundoValor, int terceiroValor) { // inicio main
         int soma = primeiroValor + segundoValor + terceiroValor; // calculo da soma
+        return soma;
+    }// fim soma
+
+    public Integer calcularSubtracao(int primeiroValor, int segundoValor, int terceiroValor) {
         int subtracao = segundoValor - primeiroValor; // Calculo subtracao
+        return subtracao;
+    }
+
+    public Integer calcularMultiplicacao(int primeiroValor, int segundoValor, int terceiroValor) {
         int multiplicacao = primeiroValor * segundoValor * terceiroValor;// Calculo multiplicação
-        int media = multiplicacao / 3; // Calculo media
+        return multiplicacao;
+    }
 
-        System.out.println("A soma dos numeros digitados é: " + soma); // Exibição soma
-        System.out.println("A subtração dos numeros digitados é: " + subtracao); // Exibição subtração
-        System.out.println("A multiplicação dos numeros digitados é:" + multiplicacao);  // Exibição multiplicação
-        System.out.println("A média dos numeros digitados é:" + media);  // Exibição media
-
-
-    }// fim main
+    public Integer calcularMedia(int primeiroValor, int segundoValor, int terceiroValor) {
+        int media = calcularMultiplicacao(primeiroValor, segundoValor, terceiroValor) / 3; // Calculo media
+        return media;
+    }
 
 }//Fim
