2f9e7c1e4fb9a06914545d921d9327186047e2a1
qaacademy <qaacademybr@gmail.com>
2024-03-02 09:47:21 -0300

    Refatoração ex4 easy

--- src/easy/ExecucaoExercicios.java
+++ src/easy/ExecucaoExercicios.java
@@ -7,11 +7,14 @@ public class ExecucaoExercicios {
 //        Exercicio02_easy exercicio02Easy = new Exercicio02_easy();
 //        System.out.println(exercicio02Easy.exibirPalavra("Estudar faz bem"));
 
-        Exercicio03_easy execicio03_ease = new Exercicio03_easy(); // Instancia da classe.
-        String primeiroValor = JOptionPane.showInputDialog("Digite o primeiro valor");
-        String segundoValor = JOptionPane.showInputDialog("Digite o segundo valor");
-        String texto = execicio03_ease.trocaValoresDigitados(primeiroValor, segundoValor);
-        System.out.println(texto);
+//        Exercicio03_easy execicio03_ease = new Exercicio03_easy(); // Instancia da classe.
+//        String primeiroValor = JOptionPane.showInputDialog("Digite o primeiro valor");
+//        String segundoValor = JOptionPane.showInputDialog("Digite o segundo valor");
+//        String texto = execicio03_ease.trocaValoresDigitados(primeiroValor, segundoValor);
+//        System.out.println(texto);
 
+        Exercicio04_easy exercicio04_easy = new Exercicio04_easy();
+        int numeroDigitado = Integer.parseInt(JOptionPane.showInputDialog("Digite um inteiro"));
+        System.out.println("O dobro do numero que você digitou é: "+ exercicio04_easy.calcularDobro(numeroDigitado)); // Exibe o valor da variavel com o um texto concatenado.
     }
 }
