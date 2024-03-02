9552eb163b65356f6dd7ce9601a6394092e6016e
qaacademy <qaacademybr@gmail.com>
2024-03-02 11:38:44 -0300

    Refatoração ex02 - medium

--- src/medium/ExecucaoMedium.java
+++ src/medium/ExecucaoMedium.java
@@ -1,10 +1,22 @@
 package medium;
 
+import javax.swing.*;
+
 public class ExecucaoMedium {
     public static void main(String[] args) {
+//
+//        Exercicio01_medium exercicio01Medium = new Exercicio01_medium();
+//        int numero = 1;
+//        System.out.println(exercicio01Medium.verificaParOuImpar(numero));
 
-        Exercicio01_medium exercicio01Medium = new Exercicio01_medium();
-        int numero = 1;
-        System.out.println(exercicio01Medium.verificaParOuImpar(numero));
+        double valorInvestimento = Double.parseDouble(JOptionPane.showInputDialog("Digite um valor de investimento"));
+        Exercicio02_medium exercicio02Medium = new Exercicio02_medium();
+        double valorTotal = exercicio02Medium.calcularInvstimentoJurosCompostos(valorInvestimento);
+
+        System.out.println("O Valor investido foi de R$ " + valorInvestimento);
+        System.out.println("O Valor total com juros foi de R$ " + valorTotal);
+        System.out.println("O Valor de juros foi de R$ " + exercicio02Medium.calcularValorDosJuros(valorTotal, valorInvestimento));
+
+
     }
 }
--- src/medium/Exercicio02_medium.java
+++ src/medium/Exercicio02_medium.java
@@ -3,18 +3,19 @@ package medium;
 import javax.swing.*;
 
 public class Exercicio02_medium {
-    public static void main(String[] args) {
-        int tempoDeInvestimento = 30;
+    public double calcularInvstimentoJurosCompostos(double valorInvestimento) {
+        int tempoDeInvestimento = 10;
         double valorTotal = 0.0;
         double taxaDeJuros = 0.12;
-        double valorInvestimento = Double.parseDouble(JOptionPane.showInputDialog("Digite um valor de investimento"));
         valorTotal = valorInvestimento;
 
         for (int i = 0; i < tempoDeInvestimento; i++) {
             valorTotal = (valorTotal * taxaDeJuros) + valorTotal;
         }
-        System.out.println("O Valor investido foi de R$ "+valorInvestimento);
-        System.out.println("O Valor de juros foi de R$ "+(valorTotal - valorInvestimento));
-        System.out.println("O Valor total com juros foi de R$ "+valorTotal);
+        return valorTotal;
+    }
+
+    public double calcularValorDosJuros(double valorTotal, double valorInvestido){
+        return (valorTotal - valorInvestido);
     }
 }
