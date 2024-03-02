a1183ec8d3048c9f0c41bfb14bfeb6b1bd4d783d
qaacademy <qaacademybr@gmail.com>
2024-03-02 09:32:43 -0300

    Refatoração Ex3 Easy

--- src/easy/ExecucaoExercicios.java
+++ src/easy/ExecucaoExercicios.java
@@ -1,8 +1,17 @@
 package easy;
 
+import javax.swing.*;
+
 public class ExecucaoExercicios {
     public static void main(String[] args) {
-        Exercicio02_easy exercicio02Easy = new Exercicio02_easy();
-        System.out.println(exercicio02Easy.exibirPalavra("Estudar faz bem"));
+//        Exercicio02_easy exercicio02Easy = new Exercicio02_easy();
+//        System.out.println(exercicio02Easy.exibirPalavra("Estudar faz bem"));
+
+        Exercicio03_easy execicio03_ease = new Exercicio03_easy(); // Instancia da classe.
+        String primeiroValor = JOptionPane.showInputDialog("Digite o primeiro valor");
+        String segundoValor = JOptionPane.showInputDialog("Digite o segundo valor");
+        String texto = execicio03_ease.trocaValoresDigitados(primeiroValor, segundoValor);
+        System.out.println(texto);
+
     }
 }
